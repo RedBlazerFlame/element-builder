50fa72b68684fb61c8b5ab13e9e3184df9246135
Asriel Dreemurr <52394592+RedBlazerFlame@users.noreply.github.com>
2020-08-30 16:52:02 +0800

    Fix Alert System Yet Again
    Why Does it keep Breaking?

--- docs/main.js
+++ docs/main.js
@@ -30,7 +30,7 @@ combineButton.addEventListener("click",()=>{
         })
         //Alert the player on the new creations
         if(res!=[]){
-            alert(`You have created the following element(s): ${JSON.stringify(res)}`);
+            alert(`You have created the following element(s): ${res.map(item=>item.name)}`);
         }
         //Updates the player progress
         localStorage.setItem("progress",JSON.stringify(elementsOwned));
